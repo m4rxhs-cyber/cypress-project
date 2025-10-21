c96ee7c2457c5f586912d53a8d14fe39da161403
m4rxhs cyber <m4rxhs3301@gmail.com>
2025-10-21 17:20:08 -0300

    adicionando novos testes e refatorando o código

--- cypress/e2e/register/registerForm.cy.js
+++ cypress/e2e/register/registerForm.cy.js
@@ -1,65 +1,111 @@
-import { RegisterForm } from '../../support/pages/RegisterForm'
+import { RegisterForm } from "../../support/pages/RegisterForm";
 
-const registerForm = new RegisterForm()
+const registerForm = new RegisterForm();
 const colors = {
-  errors: 'rgb(220, 53, 69)',
-  sucess: ''
-}
+  errors: "rgb(220, 53, 69)",
+  success: "rgb(25, 135, 84)",
+};
 
-const input ={
-  title: '', 
-  url: ''
-}
+const input = {
+  title: "",
+  url: "",
+};
 
-describe('Image Registration', () => {
-  describe('Submitting an image with invalid inputs', () => {
-    after( () => {
-      cy.clearAllLocalStorage()
-    })
 
-    it('Given I am on the image registration page', () => {
-      cy.visit('/')
-    })
+describe("Image Registration", () => {
+  describe("Submitting an image with invalid inputs", () => {
+    after(() => {
+      cy.clearAllLocalStorage();
+    });
+
+    it("Given I am on the image registration page", () => {
+      cy.visit("/");
+    });
 
     it(`When I enter "${input.title}" in the title field`, () => {
-      registerForm.typeTitle(input.title)
-    })
+      registerForm.typeTitle(input.title);
+    });
 
     it(`Then I enter "${input.url}" in the URL field`, () => {
-      registerForm.typeUrl(input.url)
-    })
+      registerForm.typeUrl(input.url);
+    });
 
     it(`Then I click the submit button`, () => {
-      registerForm.clickSubmit()
-    })
+      registerForm.clickSubmit();
+    });
 
     it(`Then I should see "Please type a title for the image" message above the title field`, () => {
-      registerForm.elements.titleFeedback().should('contains.text', 'Please type a title for the image')
-    })
+      registerForm.elements
+        .titleFeedback()
+        .should("contains.text", "Please type a title for the image");
+    });
 
     it(`And I should see "Please type a valid URL" message above the imageUrl field`, () => {
-      registerForm.elements.urlFeedback().should('contains.text', 'Please type a valid URL')
-    })
+      registerForm.elements
+        .urlFeedback()
+        .should("contains.text", "Please type a valid URL");
+    });
 
     it(`And I should see an exclamation icon in the title and URL fields`, () => {
       registerForm.elements.titleInput().should(([element]) => {
-          const styles = window.getComputedStyle(element)
-          const border = styles.getPropertyValue('border-color')
-          expect(border).to.equal(colors.errors)
-      })
+        const styles = window.getComputedStyle(element);
+        const border = styles.getPropertyValue("border-color");
+        expect(border).to.equal(colors.errors);
+      });
 
       registerForm.elements.imageUrlInput().should(([element]) => {
-        const styles = window.getComputedStyle(element)
-        const border = styles.getPropertyValue('border-color')
-        expect(border).to.equal(colors.errors)
-      })
+        const styles = window.getComputedStyle(element);
+        const border = styles.getPropertyValue("border-color");
+        expect(border).to.equal(colors.errors);
+      });
+    });
+  });
+
+  describe.only("Submitting an image with valid inputs using enter key", () => {
+    after(() => {
+      cy.clearAllLocalStorage();
+    });
+
+    input.title = "Alien BR";
+    input.url =
+      "https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg";
+
+    it(`I am on the image registration page`, () => {
+      cy.visit("/");
+    });
+
+    it(`I enter "${input.title}" in the title field`, () => {
+      registerForm.typeTitle(input.title);
+    });
+
+    it(`I should see a check icon in the title field`, () => {
+      registerForm.clickSubmit();
+
+      registerForm.elements.titleInput().should(([element]) => {
+        const styles = window.getComputedStyle(element);
+        const border = styles.getPropertyValue("border-color");
+        expect(border).to.equal(colors.success);
+      });
+    });
+
+    it(`I enter "${input.url}" in the URL field`, () => {
+      registerForm.typeUrl(input.url);
+    });
+
+    it(`I should see a check icon in the imageUrl field`, () => {
+      registerForm.elements.imageUrlInput().should(([element]) => {
+        const styles = window.getComputedStyle(element);
+        const border = styles.getPropertyValue("border-color");
+        expect(border).to.equal(colors.success);
+      });
+    });
+
+    it(`I can hit enter to submit the form`, () => {
+      registerForm.clickSubmit()
     })
-    
-  })
- 
-  // describe('Submitting an image with valid inputs using enter key', () => {
 
-  // })
-})
-
- 
+    it(`the list of registered images should be updated with the new item`, () => {
+      
+    })
+  });
+});
